121e8dd86df558a3e58f84f27e446a0c31ec1b0c
Rabinkhulimuli <lovingrabin@gmail.com>
2024-12-27 20:16:43 +0545

    hero added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
+import SearchForm from "../components/searchform";
 
-
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams: Promise<{query?:string}>
+}) {
+  const query=(await searchParams).query
   return (
     <>
-      <h1>This is homepage</h1>
+      <section  className=" pink_container">
+          <h1 className="heading" >Pitch Your Startup,<br></br>Connect with Enterpreneurs</h1>
+          <p className="sub-heading !max-w-3xl capitalize">
+            submit ideas,vote on pitches and get notice in virtual competitions.
+
+          </p>
+          <SearchForm query={query}/>
+      </section>
+      
     </>
   );
 }
