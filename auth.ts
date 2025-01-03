3c36bddf3199e09ad3bd30dcf4cbd9f9f7225491
Rabinkhulimuli <lovingrabin@gmail.com>
2025-01-03 12:09:21 +0545

    some fix

--- auth.ts
+++ auth.ts
@@ -1,15 +1,24 @@
 
-import NextAuth from "next-auth"
+import NextAuth, { User } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import { client } from "./sanity/lib/client"
 import { Author_by_github_id_query } from "./sanity/lib/queries"
 import { writeClient } from "./sanity/lib/write-client"
+import { Profile as NextAuthProfile } from "next-auth";
+
+interface GitHubProfile extends NextAuthProfile {
+  id: string;
+  avatar_url?: string;
+  login?: string;
+  bio?: string;
+}
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({user,profile}) {
+    async signIn({user,profile}:{user:User,profile?:NextAuthProfile}) {
+      const githubProfile= profile as GitHubProfile
       try {
-        const {id,avatar_url:image,name,email,bio,login}=  profile
+        const {id,avatar_url:image,name,email,bio,login}=  githubProfile
        
         const existingUser = await client.withConfig({useCdn:false}).fetch(Author_by_github_id_query, {
           id,
