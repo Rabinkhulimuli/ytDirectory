b96a274c780792b8addf63f83f917e55f2e362ab
Rabinkhulimuli <lovingrabin@gmail.com>
2025-01-08 14:03:28 +0545

    some changes

--- components/view.tsx
+++ components/view.tsx
@@ -13,7 +13,7 @@ export default async function View({id}:{id:string}){
     .set({views: totalViews + 1})
     .commit())
     return (<>
-        <div className="view-container">
+        <div className=" view-container  ">
             <div className="absolute -top-2 -right-2">
                 <Ping/>
             </div>
